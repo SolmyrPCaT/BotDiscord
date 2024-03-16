c1660032a7de682104ed0895d344f81b51f0d852
rlossec <raphaelossec@gmail.com>
2024-03-16 20:17:16 +0100

    Command to display ranked list

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 import json
 
-from utils import write_log_entry
+from utils import write_log_entry, rank
 
 load_dotenv()
 
@@ -20,6 +20,7 @@ JSON_FILE_PATH = "registrations.json"
 
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.members = True
 intents.guild_scheduled_events = True
 
@@ -88,5 +89,53 @@ async def check_event_registrations():
         json.dump(registrations, file, indent=4, default=serialize_datetime)
 
 
-bot.run(DICTABOT_TOKEN)
+async def list_rank_event_users(ctx, event_id):
+    my_guild = bot.get_guild(GUILD_ID)
+    print("rank déclenché")
+    try:
+        with open(JSON_FILE_PATH, "r") as file:
+            registrations = json.load(file)
+    except FileNotFoundError:
+        registrations = {}
 
+    for event_id_str, users_dict in registrations.items():
+        if event_id_str == str(event_id):
+            event = my_guild.get_scheduled_event(int(event_id_str))
+            event_name = event.name if event else f"Événement inconnu (ID: {event_id_str})"
+
+            sorted_users = sorted(users_dict.items(),
+                                  key=lambda x: datetime.fromisoformat(x[1]))  # Trier par date d'inscription
+            ranks = rank(dict(sorted_users))
+
+            message = f"## Ordre d'inscription : {event_name} :\n \n"
+
+            for user_id, registration_time in sorted_users:
+                rank_str = str(ranks[user_id])
+                user_id = int(user_id)
+                # Convertir la chaîne en objet datetime si nécessaire
+                if not isinstance(registration_time, datetime):
+                    registration_time = datetime.fromisoformat(registration_time)
+
+                # Obtenez l'objet membre
+                member = my_guild.get_member(user_id)
+                # Utilisez le pseudo si le membre est trouvé, sinon utilisez l'ID
+                user_name = member.name if member else f"Utilisateur inconnu (ID: {user_id})"
+                formatted_registration_time = registration_time.strftime("%d/%m/%Y %H:%M")
+                message += f" {rank_str} -- {user_name} inscrit le {formatted_registration_time}\n"
+
+            # Envoyer le message dans le canal de votre choix
+            message += "--------------------------------------------------------------------------------"
+            await ctx.send(message)
+
+
+@bot.command(name='event_users')
+async def event_users(ctx, event_id: int):
+    """
+    Affiche la liste des inscrits pour un événement spécifié.
+    Utilisation : $event_users <event_id>
+    """
+    print("rank déclenché")
+    await list_rank_event_users(ctx, event_id)
+
+
+bot.run(DICTABOT_TOKEN)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any
+
 LOG_FILE_PATH = "registration_log.txt"
 
 
@@ -7,3 +9,14 @@ async def write_log_entry(event_name, user_name, action, timestamp):
             log_file.write(f"{timestamp.isoformat()} - Event {event_name}: User {user_name} {action}\n")
     except IOError as e:
         print(f"Erreur lors de l'écriture dans le fichier de journal : {e}")
+
+
+def rank(dictionary: Dict[str, Any]) -> Dict[str, int]:
+    sorted_items = sorted(dictionary.items(), key=lambda x: x[1])
+    ranks = {}
+    current_rank = 1
+    for i, (key, value) in enumerate(sorted_items):
+        if i > 0 and value != sorted_items[i - 1][1]:
+            current_rank = i + 1
+        ranks[key] = current_rank
+    return ranks
